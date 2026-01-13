4a19009269313458bb48647f765bc6b30d5afc18
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-13 14:26:52 +0200

    Added number expressions code

---
+++ expressions-exercise/number_expressions.py
@@ -0,0 +1,23 @@
+# predictions in comments
+
+print(2 + 3) # 5
+print(10 - 15) # -5
+print(4 + 1 - 5) # 0 
+print(4 * 3) # 12
+
+print(7 / 2) # 3.5
+print(4 + 2 * 3) # 10
+print((4 + 2) * 3) # 18
+print(5 % 2) # 1 
+
+print(6 % 2) # 0
+print(7 % 2) # 1
+print(8 % 2) # 0
+print(19 % 8) # 3 
+
+print(24 % 8) # 0
+print(7 % 4) # 3 
+print(4 % 7) # 4 (if LHS is greater than RHS it just returns number on LHS)
+print(5 + (10 % 5)) # 5
+print((5 + 10) % 5) # 0
+
