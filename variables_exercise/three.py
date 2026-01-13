a096a2202d75fcb1bbacfb4f373bfc5a0421d19d
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-13 15:36:56 +0200

    Added three.py code

---
+++ variables_exercise/three.py
@@ -0,0 +1,15 @@
+# predict the outputs
+
+qty = 15 % 4
+print(qty) # 3
+
+num = 38 + 3
+num += 1
+print(str(num) + " is a great number") # 42 is a great number
+
+is_num_even = (num % 2) == 0
+print(str(num) + " is even? " + str(is_num_even)) # 42 is even? True
+
+is_qty_even = (qty % 2) == 0
+print(str(qty) + " is even? " + str(is_qty_even)) # 3 is even? False
+
