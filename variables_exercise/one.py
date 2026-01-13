fb6329acf85cc538d11184b5f74a3b8500848747
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-13 14:50:08 +0200

    Added the one.py code

---
+++ variables_exercise/one.py
@@ -0,0 +1,15 @@
+#Predicting the outputs in comments
+
+word = "bye"
+print(word + " felicia") # bye felicia
+print(word) # bye
+
+num = 10
+num = num * 2
+print(num) # 20
+
+bottles_of_beer = 99
+around = bottles_of_beer - 1
+print(around) # 98
+print(bottles_of_beer) # 99
+
