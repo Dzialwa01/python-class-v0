73f371bd79b1b6f633dce2b2138b2917f24ec4d1
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-13 14:19:51 +0200

    Added string expressions

---
+++ expressions-exercise/string_expressions.py
@@ -0,0 +1,8 @@
+# Predictions commented
+
+print("river" + "town") #rivertown
+print("cat" + "dog") #catdog
+print("New" + " York") # New York
+print("runner's knee" + "!") #runner's knee!
+print("man" + "bear" + "pig") #manbearpig
+
