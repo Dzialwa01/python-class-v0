b1e6414dc005bc190426d6bbb59568149f74e091
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-14 17:10:38 +0200

    Added the half function

---
+++ functions_exercise/01_functions/half.py
@@ -0,0 +1,13 @@
+# Write a function `half` that accepts a number as an argument.
+# The function should return half of the number.
+
+def half(num):
+    if num % 2 == 0:
+        return num // 2
+    else:
+        return num / 2
+    
+print(half(8))    # 4
+print(half(15))   # 7.5
+print(half(90))   # 45
+
