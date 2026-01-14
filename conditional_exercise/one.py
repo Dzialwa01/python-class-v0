62c78324a7dfe16ee11809a3539b8eecc60d00db
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-14 13:54:57 +0200

    Finished the one.py exercise

---
+++ conditional_exercise/one.py
@@ -0,0 +1,37 @@
+# Predict what each snippet outputs
+
+# Snippet 1.0
+
+qty = 38
+
+if qty > 30 and qty % 5 == 4:
+    print("swish") # It wont print this as both conditions should be met and are not
+else:
+    print("swoosh")  # swoosh
+
+if qty > 0:
+    print("pos") # pos
+
+
+# Snippet 1.1
+
+a = "celery"
+b = "SQUASH"
+
+if a == a.upper():
+    print("alpha")
+
+if b == b.upper():
+    print("beta") # beta
+
+
+# Snippet 1.3
+
+z = 12
+
+if z > 10:
+    print("vroom") # vroom
+
+if z % 3 == 0:
+    print("skrrt") # skrrt
+
