a894b7247c1f84779c17793fc7a145b0fecffb7d
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-14 18:36:10 +0200

    Added the funny sound function

---
+++ functions_exercise/02_functions/funny_sound.py
@@ -0,0 +1,15 @@
+# Write a function `funny_sound` that accepts two strings.
+# Return a new string made from:
+# - the first three characters of the first string
+# - the first three characters of the second string
+#
+# You may assume both strings are at least 3 characters long.
+
+def funny_sound(string1, string2):
+    return string1[:3] + string2[:3]
+
+print(funny_sound("tiger", "spoon"))       # "tigspo"
+print(funny_sound("computer", "phone"))    # "compho"
+print(funny_sound("skate", "bottle"))      # "skabot"
+print(funny_sound("frog", "ashtray"))      # "froash"
+
