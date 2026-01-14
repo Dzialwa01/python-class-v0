d82cad909e0e216e0c355774192f0220d3ae6c63
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-14 16:56:36 +0200

    Added the required function

---
+++ functions_exercise/01_functions/keep_it_quiet.py
@@ -0,0 +1,11 @@
+# Write a function `keep_it_quiet` that accepts a string as an argument.
+# The function should return the lowercase version of the string
+# with 3 periods added to the end.
+
+def keep_it_quiet(word):
+    return word.lower() + "..."
+
+print(keep_it_quiet("HOORAY"))     # "hooray..."
+print(keep_it_quiet("Doggo"))      # "doggo..."
+print(keep_it_quiet("WHAT?!?!"))   # "what?!?!..."
+
