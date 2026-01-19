77878547cb263401ac32a18dd7be4b6d1d09c9b0
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-19 14:12:11 +0200

    Added the function required successfully

---
+++ loops_exercise/A_loops_exercise/one_to_four.py
@@ -0,0 +1,13 @@
+# Dzialwa Nemakonde
+# 19 January 2025
+
+
+# Write a function `one_to_four` that prints the numbers 1 through 4 (inclusive).
+
+def one_to_four ():
+
+    for i in range (1, 5): # stop is 5 to ensure that loop ends at 4
+        print(i)
+
+one_to_four()
+
