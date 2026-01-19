4b96ab738c9ff959938453ca3ba04cc585b9b660
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-19 14:31:32 +0200

    Added the function

---
+++ loops_exercise/A_loops_exercise/string_iterate.py
@@ -0,0 +1,13 @@
+# Dzialwa Nemakonde
+# 19 January 2026
+
+# Write a function `string_iterate(text)` that prints each character of the string.
+
+def string_iterate(text):
+
+    for i in text: # i takes on each letter of the string in text woth each iteration
+        print (i)
+
+string_iterate("celery")
+string_iterate("hat")
+
