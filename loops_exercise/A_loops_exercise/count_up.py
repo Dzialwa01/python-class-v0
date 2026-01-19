48cd1eb46d9e38ecc4f289eaa73100141fa4c148
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-19 14:17:36 +0200

    Added the count_up function

--- loops_exercise/A_loops_exercise/count_up.py
+++ loops_exercise/A_loops_exercise/count_up.py
@@ -1,2 +1,13 @@
 # Dzialwa Nemakonde
 # 19 January 2026
+
+# Write a function `count_up(max_num)` that prints numbers from 1 to max_num.
+
+def count_up(num) :
+    for i in range (1, num+1): # stop is num+1 to ensure num is also printed
+        print(i)
+        
+
+count_up(5)
+count_up(3)
+
