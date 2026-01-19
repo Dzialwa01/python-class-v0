ad06a495a16beefb175ab86f2d2dbb084d218023
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-19 16:24:34 +0200

    Added the countdown function

---
+++ loops_exercise/A_loops_exercise/countdown.py
@@ -0,0 +1,19 @@
+# Dzialwa Nemakonde
+# 19 January 2026
+# Bonus 2
+
+# Write `sum_of_range(n)`
+# Print the sum of numbers from 1 to n.
+
+def countdown(num):
+
+    for i in range (5,0,-1): # Starts from 5 going down to 1
+        print(i)
+    
+countdown(5)
+# 5
+# 4
+# 3
+# 2
+# 1
+
