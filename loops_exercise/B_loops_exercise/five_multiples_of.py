f336ac2d9d6a3f3389479e14cdc8f84f3f4b500d
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-20 13:43:57 +0200

    Added the function

---
+++ loops_exercise/B_loops_exercise/five_multiples_of.py
@@ -0,0 +1,20 @@
+# Dzialwa Nemakonde
+# 20 January 2026
+
+# Write a function `five_multiples_of(n)` that prints the first five multiples of n.
+# The function does not return a value, just prints.
+
+def five_multiples_of(n):
+
+    for i in range(1, 6): # Ensuring 5 iterations with stop == 6
+        print(n*i)
+# Example:
+
+five_multiples_of(7)
+
+# 7
+# 14
+# 21
+# 28
+# 35
+
