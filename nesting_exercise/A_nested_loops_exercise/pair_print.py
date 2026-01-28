90afcc56e4565ed1e39f18b0f1135d24c3ea638c
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-01-28 14:21:27 +0200

    required function added

---
+++ nesting_exercise/A_nested_loops_exercise/pair_print.py
@@ -0,0 +1,23 @@
+# Dzialwa Nemakonde
+# 28 January 2026
+
+# Write a function `pair_print(arr)` that accepts a list and prints all unique pairs
+# of elements in the list. It doesn't need to return anything.
+
+def pair_print(arr):
+
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            print(f" {arr[i]} - {arr[j]}")
+
+# Example:
+pair_print(["artichoke", "broccoli", "carrot", "daikon"])
+
+# prints
+# artichoke - broccoli
+# artichoke - carrot
+# artichoke - daikon
+# broccoli - carrot
+# broccoli - daikon
+# carrot - daikon
+
