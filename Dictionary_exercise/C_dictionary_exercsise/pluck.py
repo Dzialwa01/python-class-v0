7c266b16fcf4c4d09123775f39cbd2abc59533a3
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-05 14:41:30 +0200

    Second task completed

--- Dictionary_exercise/C_dictionary_exercsise/pluck.py
+++ Dictionary_exercise/C_dictionary_exercsise/pluck.py
@@ -1,2 +1,36 @@
+# Dzialwa Nemakonde
+# 05 February 2026
 
+# Task:
+# Write a function `pluck` that accepts:
+# - a dictionary
+# - a list of strings
+# The function should return a new dictionary containing only the key–value pairs where:
+# - the key exists in the provided list
+
+def pluck(dict1, lst):
+
+    del_keys = []
+
+    for key in dict1.keys():
+        if key not in lst: 
+            del_keys.append(key) # storing the keys to remove as python doesn't allow you to change a dictionary while looping over it
+
+    for key in del_keys:
+        dict1.pop(key) # removing the keys not in the list    
+
+    return dict1 
+
+
+print(pluck(
+    {"name":"Fido","color":"Brown","breed":"German Shepherd" },
+    ["name","breed"]
+))
+# { "name": "Fido", "breed": "German Shepherd" }
+
+print(pluck(
+    {"make":"Tesla","mpg":93,"model":"Model X","color":"white" },
+    ["make","model"]
+))
+# { "make": "Tesla", "model": "Model X" }
 
