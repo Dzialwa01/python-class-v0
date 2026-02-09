e2cb945370b5fe36f4b2cc5d3dd7f57301c32462
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-10 00:12:10 +0200

    Errors fixed

--- Dictionary_exercise/C_dictionary_exercsise/object_add.py
+++ Dictionary_exercise/C_dictionary_exercsise/object_add.py
@@ -13,7 +13,7 @@ def object_add(dict1, dict2):
     ans_dict = {}
     both_dicts = dict1 | dict2 # merging the dictionaries to access the key's value if its not in both dictionaries
 
-    for key in common.keys(): 
+    for key in both_dicts.keys(): 
         if key in dict1.keys(): 
             if key in dict2.keys(): # checks if key is in both dictionaries
                 ans_dict[key] = dict1[key] + dict2[key]
@@ -27,13 +27,11 @@ def object_add(dict1, dict2):
 
 obj1 = {"x":3,"y":10 }
 obj2 = {"y":2,"x":1 }
-common = obj1.keys() | obj2.keys()
 print(object_add(obj1, obj2))
 # { "x": 4, "y": 12 }
 
 
 obj3 = {"a":3,"b":2,"c": -1 }
 obj4 = {"b":5,"c":1,"e":4 }
-
 print(object_add(obj3, obj4))
 # { "a": 3, "b": 7, "c": 0, "e": 4 }
