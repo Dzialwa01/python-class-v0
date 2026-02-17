e289c6d95276349155bb82ab498c61c13c2c73a9
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-17 14:38:04 +0200

    Started learning about OOP

---
+++ OOP/what_is_OOP.py
@@ -0,0 +1,45 @@
+# Dzialwa Nemakonde
+# 16 February 2026
+
+# What is Objected-Oriented Programming?
+
+# This is a way of writing programs by thinking in terms of objects. 
+# An object is something that has Properties(data) and Behaviours(actions)
+
+# Instead of writing random functions, we group data and behaviour together.
+
+# 2. Real-World Analogy
+# e.g a car
+# A car has:
+# Properties: Colour, Brand and Speed
+# Behaviours: Start, Stop and Accelerate
+# So in OOP, a car becomes an object
+
+# e.g a student
+# Properties: Name, Age and Marks
+# Behaviours: Study, Take exam and View result
+## This is exactly how OOP works. 
+
+# 3. How This Looks in Programming
+# In programming, we create a class
+# A class is a blueprint. 
+# From that blueprint, we create objects.
+# i.e Class -> Design of a house
+# Object -> Actual house
+
+# 4. Why OOP Exists:
+# Before OOP, programs were:
+# Hard to manage
+# Difficult to scale
+# Full of repeated code
+# OOP was created to solve these problems
+
+#5. Benefits of OOP
+# OOP helps us to:
+# Organise large programs
+# Reuse code easily
+# Model real-world problems
+# Write cleaner, maintainable code
+
+# THis is why OOP is used in:
+# Web applications, Mobile apps, Games and Enterprise software
