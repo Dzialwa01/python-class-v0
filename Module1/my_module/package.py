732b4262b86665bedb32d8a1921ea06fdb8747b9
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-19 16:11:28 +0200

    Learnt the difference between a module and a package

---
+++ Module1/my_module/package.py
@@ -0,0 +1,10 @@
+# Dzialwa Nemakonde
+# 18 February 2026
+
+# Packages vs Modules
+
+# A package is simply a folder of modules. 
+# It is a larger project as compared to modules
+
+# Creating a package
+# __init__.py file tells python that the folder is a package it's a package
