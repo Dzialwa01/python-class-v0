0e781012638e2af74a099763d5c8653dbae9c296
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-19 22:32:15 +0200

    Learnt about knowledge bots

---
+++ Module 2/knowledge_bots.py
@@ -0,0 +1,45 @@
+# Dzialwa Nemakonde 
+# 19 February 2026
+
+# 1. What is a knowledge Bot
+# A knowledge bot is a smart helper that answers questions using information you give it. (Using company documents)
+#E.g School chatbot, Bank WhatsApp chatbot, Hospital/clinic information, company helpdesk bot
+# Simple analogy - A knowledge bot is like a student studying notes. it can only answer from what it has learned.
+
+# 2. Why do businesses use knowledge bots?
+# To answer questions faster and also many people at the same time
+# Help customers 24/7
+# Help new employees learn
+# Reduce training time 
+# Save money and time
+
+## Example tasks:
+# Task - How a bot helps 
+# Ask school fees - Bot tells fee info
+# Ask HR about leave - Bot gives policy
+# Ask shop about product - Bot give details
+
+# 3. Business Knowledge (Simple IP)
+# There are two types of information: 
+# Type - Meaning - Example
+# Public info - Anyone can see - website,brochurs
+# Private info / company secrets - Only for business - staff rules, price,lists
+# Rule for students - Never put private company info into public AI tools
+
+# 4. When AI Can Be Wrong (Hallucinations)
+# Sometimes AI sounds right but is wrong, this is called Hallucinations
+# To avoid this : use real company documents, update information and check answers before trusting them.
+
+# Simple Life Cycle of a Bot (Beginner View)
+# Step : Simple Description
+# Collect info : Get correct documents]
+# Organise : Put them in folders
+# Give to bot system : system reads them
+# Bot answers : Bot uses that info to to reply
+
+# Question - Answer
+# What is a knowledge bot? - Smart helper that answers using information you give it
+# Public vs private info? - Public = everyone can see; Private = company only
+# Why do companies use bots? - Save time, answer fast, help customers
+# Why can AI be wrong sometimes? - It can guess or use old info
+# What should you never do? - Upload private info to public AI
