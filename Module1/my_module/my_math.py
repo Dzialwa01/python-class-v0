a524ad1cb70e36351d614312eddbb55b385a6396
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-02-19 16:29:04 +0200

    Created a package

---
+++ Module1/my_module/my_math.py
@@ -0,0 +1,11 @@
+# Dzialwa Nemakonde
+# 18 February 2026
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    if x > y:
+        return x - y
+    else:
+        return y - x
---
+++ Module1/my_module/my_script.py
@@ -0,0 +1,9 @@
+# Dzialwa Nemakonde
+# 18 February 2026
+
+# Testing if the package works
+
+from my_module.my_math import add, subtract
+
+print(add(6,5))
+print(subtract(8, 15))
