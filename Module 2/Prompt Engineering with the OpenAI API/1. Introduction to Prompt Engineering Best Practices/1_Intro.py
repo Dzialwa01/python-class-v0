b64ce91d1713e67a50930cc5a360ef929120b379
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-04-13 00:22:03 +0200

    Got introduced into prompt engineering

---
+++ Module 2/Prompt Engineering with the OpenAI API/1. Introduction to Prompt Engineering Best Practices/1_Intro.py
@@ -0,0 +1,23 @@
+# Dzialwa Nemakonde
+# 09 March 2026
+
+## Introduction to Prompt Engineering Best Practices
+
+# What is Prompt Engineering?
+# Prompt engineering means writing clear and well - structured instructions (prompts) for AI models to get the output we want.
+# Examples: Poor prompt - Explain python
+# : Better prompt- Explain Python in simple words with one example for beginners
+
+
+## Prompt Engineering is Like a Recipe
+
+# Prompt engineering is compared to cooking a meal.
+# The ingredients are the words in your prompt, Recipe is the structure of your prompt and the Dish is the AI response
+# The key idea is to carefully choose words, instructions ana examples just like a chef would carefully choose ingredients.
+# Examples: Make food -> This is confusing
+# : Make a cheese pizza with thin crust -> You'll get exactly what you want.
+
+
+## Why Prompt Engineering is Important
+
+# Prompt engineering is important because the better the prompts, the better the quality of the answers.
