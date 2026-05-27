0a796226a7908c8c08c140a5d77e729fd9f76761
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-05-27 21:01:07 +0200

    Learnt about few-shot prompting

---
+++ Module 2/Prompt Engineering with the OpenAI API/2. Advanced Prompt Engineering Strategies/few_shot_prompting.py
@@ -0,0 +1,26 @@
+# Dzialwa Nemakonde
+# 11 May 2026
+
+# Few shot prompting- Guiding AI by showing examples instead of only giving instructions
+
+## What is few shot prompting?
+# This means giving examples inside the prompt before asking the real question.
+# Usually includes an example question, example answer, then our actual question.
+
+
+## How few-shot prompting works:
+# We write a prompt with examples, we send it to the model, the model copies the pattern from the examples and then it answers our final question in the same way.
+
+
+## Types of prompting
+
+# 1. Zero-Shot Prompting -> This is giving a direct question with no examples
+# The model answers using its existing knowledge only. It is best for simple questions, definitions, and quick answers.
+
+# 2. One-shot-prompting -> THs is giving one example which the model learns the format or style from.
+# THe one example ensures the model uses that same structure everytime.
+
+# 3. Few-shot prompting ->This is giving multiple examples in the prompt.
+# This is useful for complex tasks as the model learns how to classify texts from multiple examples.
+
+# In Few-shot prompting, we ensure to give clear examples, cover all categories and use diverse examples.
