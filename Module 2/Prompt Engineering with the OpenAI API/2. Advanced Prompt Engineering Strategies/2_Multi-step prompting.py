bf160b8c9336ea2ac437abd1d57a8ea8d9f93771
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-05-28 04:31:59 +0200

    Learnt about multi-step prompting

---
+++ Module 2/Prompt Engineering with the OpenAI API/2. Advanced Prompt Engineering Strategies/2_Multi-step prompting.py
@@ -0,0 +1,33 @@
+# Dzialwa Nemakonde
+# 12 May 2026
+
+# Multi-step prompting: This technique helps us guide AI step by step so it gives better and more accurate answers.
+
+## What is Multi-Step Prompting?
+# This means breaking on big tasks into smaller steps. And we tell the model what to do in each step
+# This works very well for sequential taks and thinking tasks (Analysis, checking, decision-making)
+
+# Just like humans, AI performs better when instructions are clear and ordered:
+# Examples of where it helps include: Writing an article from an outline, Solving a problem step by step, Checking whether code is correct.
+
+
+## Multi-step prompt Example (Writing a Blog)
+# Step1: Introduce the destination.
+# Step2: Describe a personal adventure.
+# Step3: Summarize the journey.
+# In these step, we did not specify the destination but rather focused on the destination.
+# This makes the blog to be generated well organised, easy to read and more professional.
+
+
+## Multi-Step prompting for Thinking tasks
+# Multi-step prompting can be great for analysis tasks like checking if code is correct, finding logical errors or evaluating solutions.
+# e.g., 
+# Step1: Check if the code syntax is correct.
+# Step2: Check if division by zero is handled.
+# This guarantees that we obtain clear feedback, logical reasoning and better evaluation.
+
+
+## Comparison between Multi-Step Prompt and Few-shot prompt
+# Multi-step prompt uses instructions to tell the model what to do, like a roadmap.
+# Few-shot prompting uses examples to show the model how to respond which is like learning by example
+# The best results often come when both are combined.
