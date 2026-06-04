eca974d282fcd7ce66326b345832552a3849469c
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-06-04 10:33:47 +0200

    Learnt about Text summarisation and expansion

---
+++ Module 2/Prompt Engineering with the OpenAI API/3. Prompt Engineering for Business Applications/1_Text_summarisation_&_expansion.py
@@ -0,0 +1,36 @@
+# Dzialwa Nemakonde 
+# 18 May 2026
+
+# Text Summarization and Text Expansion
+
+## What it Text Summarization?
+# This is simply taking a long text and converting it into a short, clear version without losing the main point of the text
+# This is particularly prominent in finance, Marketing, Business & Office work.
+
+## Improving summarization prompt.
+# Just telling the model to summarize something, may lead to results that are still to long or unclear.
+# These prompts can be improved by adding Output limits, output structure and the summarization focus.
+
+# Output limits - We tell the model how long the summary should be i.e., Number of sentences, words or characters.
+# E.g., "Summarize the following review in one sentence"
+
+# Output Structure: We tell the model how the output should look i.e., Bullet points, Numbered list or Paragraph
+# E.g., Summarize the review in at most three bullet points.
+
+# Summarization focus - We tell the model what to focus on while summarizing
+# E.g., Summarize the review  in three sentences, focusing on key features and user experience
+# This ensures that the model ignores unneccesary details which leads to better summaries
+
+
+## What it Text Expansion?
+# This is simply taking short ideas and expanding them into detailed, meaningful text.
+# This is often used utilised in writing emails, Creating service descriptions, Expanding reports and also generating marketing content.
+
+## Key elements of a text expansion prompt
+# A good expansion prompt should mention:
+# - What text to expand 
+# - Focus areas
+# - Output rules: Tone (professional, friendly); Length (Sentences or paragraphs); Audience (Customers, managers)
+
+# Example: Given points: Content creation, Community engagement and audience engagement
+# "Expand the following points intp a professional two-sentence description explaining the features and benefits of the service."
