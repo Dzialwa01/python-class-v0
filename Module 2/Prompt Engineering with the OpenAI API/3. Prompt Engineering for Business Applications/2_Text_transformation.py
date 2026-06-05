6c1c091cf4dd3c99634224b274f9cfd97de7f05c
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-06-05 09:32:13 +0200

    Learnt about text transformation

---
+++ Module 2/Prompt Engineering with the OpenAI API/3. Prompt Engineering for Business Applications/2_Text_transformation.py
@@ -0,0 +1,31 @@
+# Dzialwa Nemakonde 
+# 18 May 2026
+
+# Text Transformation - This is changing a given text into a new version without changing its original meaning. The information stays the same, but the form, language, tone, or quality changes.
+
+# Common Text Trasformation tasks - Language translation, Tone adjustment, Audience-based rewriting, Grammer correction, Written improvement, Multiple transformations together
+
+## Language translation
+# When asking the model to translate text, always clearly mention the input language and the output language.
+# e.g, "Translate the following product description from English to French: 'This electric scooter offers a smooth ride, long battery life, and fast charging.'"
+# If the input language is not known, we can ask the model to identify it first.
+# We can also ask the model to translate the text into multiple languages at the same time.
+
+
+## Tone Adjustment
+# This is rewriting the same message so that it can match a different style or mood.
+# Examples of tone are: Informal, Formal, Professional, Persuasive And Friendly
+# Tone depends a lot on who the audience is, so we can adjust it so it matches the specific audience.
+
+
+## Grammer and Writing improvements
+# Proofreading - This is done when we want to: fix spelling, grammatical or punctuation errors, without changing the style or structure of the original text.
+# Restructuring - This is done when we want better clarity, flow and better readability
+
+
+## Multiple Transformations (Multi-Step Prompts)
+# The model can be asked to perform multiple tasks in sequence.
+# e.g. 
+# """Step 1: Proofread the following text and fix all grammer and spelling errors.
+# Step 2: Rewrite the corrected text in a professional tone
+# Text: "this product is really good i like it a lot works great"""
