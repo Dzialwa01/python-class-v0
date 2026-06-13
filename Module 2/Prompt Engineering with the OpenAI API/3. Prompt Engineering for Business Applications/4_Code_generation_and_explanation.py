3d0332c01f064db4e3f169d05d6621b47b167971
Dzialwa Nemakonde <dzialwanemakonde42@gmail.com>
2026-06-13 16:49:42 +0200

    Learnt about code generation and code explanation using effective prompts

---
+++ Module 2/Prompt Engineering with the OpenAI API/3. Prompt Engineering for Business Applications/4_Code_generation_and_explanation.py
@@ -0,0 +1,37 @@
+# Dzialwa Nemakonde
+# 20 May 2026
+
+# Code generation and explanation using effective prompts
+# This is a powerful application of LLMs that helps developers write and understand code faster.
+
+
+## Code Generation
+# This means creating code that solves a specific problem.
+# Note: LLMs generate code quickly but one must undertand the code first before using it in real projects.
+
+# Writing effective code generation prompts.
+# A good code-generation prompt should clearly include:
+# The problem description, The programming language and the desird format(function, script, class, e.t.c)
+# E.g - "Write a python function to calculate the average sales per quarter."
+
+# Sometimes, instead of describing the problem, we provide input-output examples and the model figures out the task to perform.
+# E.g., Input: [100, 200, 300, 400]; Output: 250
+# The model does this by understanding the patterns in inputs and outputs, identifies the underlying logic, generates the appropriate function and then demonstrates how it works using the given examples.
+
+# We can also ask the model to modify existing code instead of creating completely new ones. And we can even request multiple changes at once.
+
+
+## Code Explanation
+# As code can sometimes be long, complex and hard to understand, LLMs are able to explain code quickly and clearly.
+
+# When asking for an explanation, we should specify:
+# - Length of explanation (one sentence, paragraph, detailed)
+# - Depth (high-level or step-by-step)
+
+# How detailed Explanation works
+# The model
+# - Explains the function definition
+# - Describes how inputs are processed
+# - Explains calculations step by step
+# - Describes the return value
+# - Summarizes the overall purpose of the code
